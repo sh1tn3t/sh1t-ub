58e78e1b43eae1e787245a03534645df65a37cc1
Stanislaw <fliyd206@gmail.com>
2021-10-30 09:29:54 +0600

    версия 0.5: добавлена настройка конфига

--- sh1t-ub/__main__.py
+++ sh1t-ub/__main__.py
@@ -1,17 +1,34 @@
+import os
 import logging
+import configparser
+
 from . import main, logger
 from .misc import app, db
 
 
 
+def config_setup():
+    config = configparser.ConfigParser()
+    config["pyrogram"] = {
+        "api_id": input("Введи свой API ID: "),
+        "api_hash": input("Введи свой API hash: "),
+    }
+
+    with open("config.ini", "w") as file:
+        config.write(file)
+
+
 def cli():
+    if not os.path.exists("config.ini"):
+        config_setup()
+
     logger.setup_logger(ignored = [
         "pyrogram.session",
         "pyrogram.connection",
         "pyrogram.methods.utilities.idle"
     ])
-    logging.info("Started successful")
 
+    logging.info("Started successful")
     app.run(main.main(app, db))
     logging.info("Shutting down...")
 
